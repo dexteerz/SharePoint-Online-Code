66c5a6e33cec63f0590207147cdac63c3269bcae
Breno Teixeira Fonseca <inf.teixeira@gmail.com>
2017-03-24 11:29:47 -0300

    uppercase add field in form sharepoint

--- js/listNewEdit_upperCase.js
+++ js/listNewEdit_upperCase.js
@@ -1,25 +1,30 @@
 <script language="javascript" src="https://code.jquery.com/jquery-1.4.2.min.js" type="text/javascript" ></script>
 <script language="javascript" src="https://jquery-joshbush.googlecode.com/files/jquery.maskedinput-1.2.2.min.js" type="text/javascript" ></script>
-<script language="javascript" type="text/javascript">
-/* 
-	~~ TECNO CENTER: bTEIXEIRA ~~
 
->>	Onde possui CAMPO, remover e por o nome exato do campo da Lista
->>	Para campos obrigatórios adicionar "Campo Obrigatório" 
-	-> Ex.: $("input[title=CAMPO Campo Obrigatório]").keyup(function() {
->>	Caso possua mais de um campo a qual deseja aplicar o UpperCase, basta copiar o comando todo e alterar o nome do CAMPO
-	-> Ex.:
-		$(document).ready(function(){
-        $("input[title=TESTE]").keyup(function() {
-        $(this).val($(this).val().toUpperCase());
-        });
-        });
+<script language="javascript" type="text/javascript">
+//
+/*	~~ TECNO CENTER: bTEIXEIRA ~~*/
+//
+
+/*
+>>	Changing the LISTFIELD Field to the Correct Field Name
+>>	For required fields add "Requered Field" or "Campo Obrigatório" 
+	-> Ex.: $("input[title=LISTFIELD Campo Obrigatório]").keyup(function() {
 */
 
-$(document).ready(function(){
-        $("input[title=CAMPO]").keyup(function() {
-        $(this).val($(this).val().toUpperCase());
-        });
+        $(document).ready(function(){
+                $("input[title=LISTFIELD]").keyup(function() {
+                        $(this).val($(this).val().toUpperCase());
+                });
         });
 
+        /* ADD NEW UPPERCASE */
+        /* 
+                ->
+                $(document).ready(function(){
+                       $("input[title=LISTFIELD]").keyup(function() {
+                               $(this).val($(this).val().toUpperCase());
+                        });
+                });
+        */
 </script>
