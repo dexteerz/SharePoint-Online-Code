140a5ba2bfe2c48b28b4b5520228a746d3d2d95a
Breno Teixeira Fonseca <inf.teixeira@gmail.com>
2017-03-24 08:54:42 -0300

    ATTACH CHANGE
    ALERT

--- js/nochange/attach-notnull-listNewEdit.js
+++ js/nochange/attach-notnull-listNewEdit.js
@@ -1,10 +1,12 @@
+
 <script type="text/javascript"  language="javascript">
+/* TOGETHER THE ANNEX OF A LIST OF COMPULSORY SHAREPOINT */
 /* ~~ TECNO CENTER: bTEIXEIRA ~~ */ 
 function PreSaveAction() {
 	var elm = document.getElementById("idAttachmentsTable");
 	if (elm == null || elm.rows.length == 0){
 		document.getElementById("idAttachmentsRow").style.display='none';
-		alert("ANEXO OBRIGATÓRIO, INSIRA O ARQUIVO");
+		alert("ALERT MESSAGE");
 		return false ;
 	} else { 
 		return true ;
