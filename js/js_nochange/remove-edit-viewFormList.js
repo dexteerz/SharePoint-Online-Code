cfc23df4741ebd3d894dbb80bfb34b044c5bb1a3
Breno Teixeira Fonseca <inf.teixeira@gmail.com>
2017-12-27 14:54:57 -0300

    #Form-View-List

---
+++ js/js_nochange/remove-edit-viewFormList.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+    function hideEdit() {
+        var edit = document.getElementById("Ribbon.ListForm.Display.Manage.EditItem-Large");
+        edit.style.display = "none";
+    }       
+    _spBodyOnLoadFunctionNames.push("hideEdit");
+</script>​<br/>
